66eeaa0705086392cc15e4077c35a07fa8f8a00b
Tia Rose <shortyshort789@aol.com>
2019-03-14 12:07:13 -0700

    Finally got both guessing games working, still need to create an alert at the end of the game

--- js/app.js
+++ js/app.js
@@ -13,119 +13,64 @@ do{
     turns--;
     alert('Too low, try again. You have ' + turns + ' remaining');
     guess =parseInt(prompt ('What is my number?'));
-  }   if (turns <= 0) {
+  } if (turns <= 0) {
     alert('Hey! I said you were out of turns cheater');
   } if (guess === myNum){
     alert('OMG! You are right!'); 
-    }
   }
+}
 while (guess !== myNum && turns >= 1);
 
 
 
 
-// Need to still have user attempt 5 more times and count how many guesses it took
+// // Need to still have user attempt 5 more times and count how many guesses it took
 
 var countriesLived = ['united states', 'scotland', 'england', 'italy'];
+var arrayTurns = 5;
 var index = -1;
+var user = prompt('What countries have I lived in?').toLowerCase();
 
 do{
-  var user = prompt('What countries have I lived in?').toLowerCase();
-    for(var i = 0; i < countriesLived.length; i++){
-      if (countriesLived[i] === user){
-        index = i;
-       // break;
-      }
-    }
-    if (index > -1){
-      alert('You guessed: ' + user + '. That\'s right!');
+  for(var i = 0; i < countriesLived.length; i++){
+    if (countriesLived[i] === user){
+      arrayTurns--;
+      alert('You guessed: ' + user + '. That\'s right! Now you have ' + arrayTurns + ' turns left.');
+      user = prompt('Guess again!');
+      index = i;
+      // break;
     }else{
-      alert('Sorry, you didn\'t get any right');
+      arrayTurns--;
+      alert('Sorry, you didn\'t get it right. Now you have ' + arrayTurns + ' turns left.');
+      user = prompt('Try again!');
     }
+  // if (index > -1){
+  //   alert('You guessed: ' + user + '. That\'s right!');
+  // }else{
+  //   alert('Sorry, you didn\'t get any right');
+  }
 } 
-while(index === -1);
+while(turns < 0);
 
 
 
 
+// var countriesLived = ['united states', 'scotland', 'england', 'italy'];
+// var arrayTurns = 5;
+// var index = -1;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//5 functions tied to corresponding buttons that display prompt and alert after user types in answer
-
-// function questionOne(){
-//   var isScaredOfDogs = prompt('Am I scared of big dogs?');
-//   if (isScaredOfDogs.toLowerCase() === 'yes' || isScaredOfDogs.toLowerCase() === 'y'){
-//     alert('You are right!');
-//   }else{
-//     alert('Sorry, buddy that\'s incorrect');
+// do{
+//   var user = prompt('What countries have I lived in?').toLowerCase();
+//   for(var i = 0; i < countriesLived.length; i++){
+//     if (countriesLived[i] === user){
+//       index = i;
+//       // break;
+//     }
 //   }
-//   console.log(isScaredOfDogs);
-// }
-
-// function questionTwo(){
-//   var likesFootball = prompt('Do I enjoy watching football?')
-//   if(likesFootball.toLowerCase() === 'yes' || likesFootball.toLowerCase() === 'y'){
-//     alert('You know me so well!');
+//   if (index > -1){
+//     alert('You guessed: ' + user + '. That\'s right!');
 //   }else{
-//     alert('Wow, why are you even taking this quiz');
+//     alert('Sorry, you didn\'t get any right');
 //   }
-//   console.log(likesFootball);
 // }
-
-// function questionThree(){
-//   var likesCheese = prompt('Do I like cheese?');
-//   if(likesCheese.toLowerCase() === 'no' || likesCheese.toLowerCase() === 'n'){
-//     alert('Wow, we really are besties');
-//   }else{
-//     alert('Ewwww cheese, gross');
-//   }
-//   console.log(likesCheese);
-// }
-
-// function questionFour(){
-//   var likesWorkingOut = prompt('Do I like working out?');
-//   if(likesWorkingOut.toLowerCase() === 'no' || likesWorkingOut.toLowerCase() ==='n'){
-//     alert('You got that right!');
-//   }else{
-//     alert('Sorry, I am the laziest person ever');
-//   }
-//   console.log(likesWorkingOut);
-// }
-// function questionFive(){
-//   var likesDoughnuts = prompt('Do I like doughnuts?');
-//   if(likesDoughnuts.toLowerCase() === 'yes' || likesDoughnuts.toLowerCase() === 'y'){
-//     alert('Mmmmmm doughnuts');
-//   }else{
-//     alert('Who doesn\'t like doughnuts');
-//   }
-//   console.log(likesDoughnuts);
-// }
+// while(index === -1);
